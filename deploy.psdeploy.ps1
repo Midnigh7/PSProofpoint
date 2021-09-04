4ebe9f58d6a0a0edad9ac780e413f0097612f48d
Nick Lepore <Nick.Lepore@gmail.com>
2021-09-03 22:54:49 -0500

    !deploy

--- deploy.psdeploy.ps1
+++ deploy.psdeploy.ps1
@@ -1,4 +1,4 @@
-if($ENV:BHProjectName -and $ENV:BHProjectName.Count -eq 1)
+if($ENV:BHProjectName)
 {
     Deploy Module {
         By PSGalleryModule {
@@ -9,4 +9,14 @@ if($ENV:BHProjectName -and $ENV:BHProjectName.Count -eq 1)
             }
         }
     }
-}
+}else{
+    "Skipping deployment: To deploy, ensure that...`n" +
+    "`t* ProjectName $ENV:BHProjectName`n" ##+
+<#    "`t* ENV:BHModulePath is set (Current [$([bool]$env:BHPSModulePath)]: $ENV:BHPSModulePath)`n" +
+    "`t* You are in a known build system (Current [$($env:BHBuildSystem -ne 'Unknown')]: $ENV:BHBuildSystem)`n" +
+    "`t* You are committing to the master branch (Current [$($env:BHBranchName -eq "master")]: $ENV:BHBranchName) `n" +
+    "`t* Your commit message includes !deploy (Current [$($env:BHCommitMessage -match '!deploy')]: $ENV:BHCommitMessage)" |
+ #>
+    Write-Host
+
+    }
